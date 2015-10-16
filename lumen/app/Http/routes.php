99eda31c0bedf0ea2900e15a2500cfd6d527a3b0
Thor Even Tutturen <t.tutturen@gmail.com>
2015-10-16 19:05:47 +0200

    fix error in routes

--- lumen/app/Http/routes.php
+++ lumen/app/Http/routes.php
@@ -3,5 +3,5 @@
 $apiUrl = "api/v1";
 
 $app->get($apiUrl.'/start',          'MainController@start');
-$app->post($apiUrl.'api/statements', 'MainController@getStatements');
-$app->post($apiUrl.'api/result',     'MainController@getResult');
+$app->post($apiUrl.'/statements', 'MainController@getStatements');
+$app->post($apiUrl.'/result',     'MainController@getResult');
