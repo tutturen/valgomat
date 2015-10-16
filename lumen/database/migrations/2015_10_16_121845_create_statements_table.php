67c8799634e0781e4a2dc36b7469c9c10e1fea7f
Thor Even Tutturen <t.tutturen@gmail.com>
2015-10-16 14:19:28 +0200

    add statements table migration

---
+++ lumen/database/migrations/2015_10_16_121845_create_statements_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('text');
+            $table->integer('topicId');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('statements');
+    }
+}
