564f8700a77eec4c5f44ffee7dde98660e8668a4
Thor Even Tutturen <t.tutturen@gmail.com>
2015-10-16 14:11:56 +0200

    add timestamps to users table

--- lumen/database/migrations/2015_10_16_115627_create_users_table.php
+++ lumen/database/migrations/2015_10_16_115627_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
           $table->integer('ageGroup');
           $table->integer('partyLastElection');
           $table->integer('municipality');
+          $table->timestamps();
         });
     }
 
