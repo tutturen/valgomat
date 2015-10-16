8dc2f2ccb1c6929dd28dbede5d8e60ef9bfdaacb
Thor Even Tutturen <t.tutturen@gmail.com>
2015-10-16 19:20:44 +0200

    fix spacing in routes

--- lumen/app/Http/routes.php
+++ lumen/app/Http/routes.php
@@ -2,6 +2,6 @@
 
 $apiUrl = "api/v1";
 
-$app->get($apiUrl.'/start',          'MainController@start');
+$app->get($apiUrl.'/start',       'MainController@start');
 $app->post($apiUrl.'/statements', 'MainController@getStatements');
 $app->post($apiUrl.'/result',     'MainController@getResult');
